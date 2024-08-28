9a9892621ef67a9b594d210a502adb85f3bc6774
IftakherAlam01 <iftakheralam.ewu@gmail.com>
2024-08-28 19:30:12 +0600

    created basic structure and server side

---
+++ client/src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
+
+import './App.css';
+
+const cookies = new Cookies();
+
+const apiKey = 'e765sxsmeccs';
+const authToken = cookies.get('token');
+
+const client = StreamChat.getInstance(apiKey);
+
+if (authToken) {
+    client.connectUser({
+        token:cookies.get('token'),
+        username:cookies.get('username'),
+        fullName:cookies.get('fullName'),
+        phoneNumber:cookies.get('phoneNumber'),
+        avatarURL:cookies.get('avatarURL'),
+        hashedPassword:cookies.get('hashedPassword'),
+    })
+}
+const App = () => {
+
+    if(!authToken) return <Auth/>
+
+    return (
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer
+                
+                />
+
+                <ChannelContainer
+                    
+                />
+            </Chat>
+            
+
+        </div>
+    );
+}
+
+export default App;
